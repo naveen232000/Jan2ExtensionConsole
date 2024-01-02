0f025f418daaa33e7d187e72961bd2f0f8ad0529
mygit@23 <naveenmadhesan23@gmail.com>
2024-01-02 15:04:18 +0530

    Add project files.

---
+++ Jan2ExtensionConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Jan2ClassLibrary;
+namespace Jan2ExtensionConsole
+{
+   static class AddCourse
+    {
+        public static void Csc(this College college)
+        {
+            Console.WriteLine("Available CSC Course Now!");
+        }
+        public static void Mtech(this College college)
+        {
+            Console.WriteLine("Available Mtech Course Now!");
+        }
+    }
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            College college = new College();
+            college.Ece();
+            college.Eee();
+            college.Mech();
+            college.Csc();
+            college.Mtech();
+            Console.ReadLine();
+        }
+    }
+}
